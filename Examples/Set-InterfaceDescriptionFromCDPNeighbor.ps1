400cee54d47d807bbb6b7ff4a575b3af09223a7d
lahell <47929663+lahell@users.noreply.github.com>
2019-07-12 22:17:52 +0200

    Create Set-InterfaceDescriptionFromCDPNeighbor.ps1
    This script will get hostname for all CDP neighbors and set it as interface description.

---
+++ Examples/Set-InterfaceDescriptionFromCDPNeighbor.ps1
@@ -0,0 +1,19 @@
+﻿# Prepare a hashtable with parameters
+$Request = @{
+    UserName   = 'MyUser'
+    Target     = 'MySwitch'
+    AuthType   = 'SHA1'
+    AuthSecret = 'MyAuthSecret'
+    PrivType   = 'AES256'
+    PrivSecret = 'MyPrivSecret'
+}
+
+# Walk cdpCacheDeviceId
+Invoke-SNMPv3Walk -OID 1.3.6.1.4.1.9.9.23.1.2.1.1.6 @Request | foreach {
+    $Interface = $_.OID -split '\.' | select -SkipLast 1 | select -Last 1
+    $OID = '{0}.{1}' -f '1.3.6.1.2.1.31.1.1.1.18', $Interface
+    $Description = $_.Value -split '\.' | select -First 1
+
+    # Set ifAlias
+    Invoke-SNMPv3Set -OID $OID -Type String -Value $Description @Request    
+}
